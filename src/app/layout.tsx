1990604c6cd430640f149dd16cb9d19d2686a9c2
Ryasiro <156523633+Ryasiro@users.noreply.github.com>
2024-11-29 17:47:04 +0700

    2- Convex basics implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,8 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-	{children}
-</ThemeProvider>
+	      <ConvexClientProvider>{children}</ConvexClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/dummy-data/db.ts
+++ src/dummy-data/db.ts
@@ -2,7 +2,7 @@ export const conversations = [
 	{
 		_id: "1",
 		admin: "user1",
-		groupImage: null,
+		groupImage: "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1634025439/01h5rz5b325xgav0w39tdc4kmg.jpg",
 		groupName: "Group A",
 		participants: ["user1", "user2", "user3"],
 		_creationTime: 1638232272, // Unix timestamp for 2021-11-30 12:04:32 UTC
@@ -18,14 +18,14 @@ export const conversations = [
 	{
 		_id: "2",
 		admin: null,
-		groupImage: "https://avatars.githubusercontent.com/u/75279146?v=4",
-		groupName: null,
+		groupImage: "https://static.miraheze.org/tampvanwiki/0/0a/Rusdi_Omaygot.png",
+		groupName: "obed",
 		participants: ["user4", "user5"],
-		_creationTime: 1638235872, // Unix timestamp for 2021-11-30 13:04:32 UTC
+		_creationTime: 2048072591, // Unix timestamp for 2021-11-30 13:04:32 UTC
 		lastMessage: {
 			_id: "2",
 			messageType: "text",
-			content: "Hey there!Hey there!Hey there!Hey there!Hey there!Hey there!Hey there!Hey there!",
+			content: "ak sk obed",
 			sender: "user2",
 		},
 		sender: "user4",
@@ -34,8 +34,8 @@ export const conversations = [
 	{
 		_id: "3",
 		admin: null,
-		groupImage: null,
-		groupName: null,
+		groupImage: "https://i.pinimg.com/736x/be/3b/1e/be3b1e0db11cd0eaf765dae647d939fd.jpg",
+		groupName: "syafik",
 		participants: ["user6", "user7"],
 		_creationTime: 1638239472, // Unix timestamp for 2021-11-30 14:04:32 UTC
 		lastMessage: {
@@ -51,8 +51,8 @@ export const conversations = [
 		_id: "4",
 		admin: null,
 		groupImage:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-		groupName: null,
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTybDrCd8of9PxlwDHA6ui_xPzgX0eBY-v-Hg&s",
+		groupName: "arep",
 		participants: ["user8", "user9", "user10"],
 		_creationTime: 1638243072, // Unix timestamp for 2021-11-30 15:04:32 UTC
 		lastMessage: {
@@ -96,24 +96,24 @@ export const messages = [
 export const users = [
 	{
 		_id: "user1",
-		name: "John Doe",
+		name: "Syafik",
 		email: "johndoe@email.com",
-		image: "https://randomuser.me/api/portraits/men/67.jpg",
+		image: "https://static.miraheze.org/tampvanwiki/0/0a/Rusdi_Omaygot.png",
 		admin: true,
 		isOnline: true,
 	},
 	{
 		_id: "user2",
-		name: "Jane Doe",
+		name: "Arep",
 		email: "janedoe@email.com",
-		image: "https://randomuser.me/api/portraits/women/67.jpg",
+		image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0gvNfxYfxjK6VUxAkepwgVCxKsd7Edj-NhJt6y-D7HQf-VOygXAE4iXikSXMI4_rQWd4&usqp=CAU",
 		isOnline: true,
 	},
 	{
 		_id: "user3",
-		name: "Alice",
+		name: "Arya",
 		email: "alice@email.com",
-		image: "https://randomuser.me/api/portraits/women/68.jpg",
+		image: "https://blue.kumparan.com/image/upload/fl_progressive,fl_lossy,c_fill,q_auto:best,w_640/v1634025439/01h5rz5b325xgav0w39tdc4kmg.jpg",
 		isOnline: false,
 	},
 ];
